2b642216a2ce72474c57f17ea5177bad3c04a5ab
Rasmitha kankati <rasmithadev@gmail.com>
2026-05-12 19:35:19 +0530

    switch

---
+++ index.js
@@ -0,0 +1,17 @@
+let week=7;
+ switch(week) {
+    case 1:
+        console.log("monday");
+        break;
+    case 2:
+        console.log("tuesday");
+        break;
+    case 3:
+        console.log("wed");
+        break;
+    case 4:
+        console.log("thur");
+        break;
+    default:
+        console.log("not available");
+ }
