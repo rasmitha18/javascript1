6f4d0e47a3473a4c309043940f152f8d29343247
Rasmitha kankati <rasmithadev@gmail.com>
2026-05-13 19:54:35 +0530

    click event

---
+++ click.js
@@ -0,0 +1,9 @@
+let button=document.getElementById("head");
+ button.addEventListener("click" ,function() 
+ {
+ button.innerText="hello";
+ document.body.style.color="orange";
+ document.body.style.backgroundColor="black";
+ document.body.style.textAlign="center";
+ document.body.style.fontSize="15px";
+ });
